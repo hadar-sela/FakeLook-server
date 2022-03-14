9c83ff91fdfc05f2eec809b856671d3144263f96
hadar-sela <101385170+hadar-sela@users.noreply.github.com>
2022-03-14 16:41:18 +0200

    UserController UserRepository

---
+++ fakeLook-starter/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using fakeLook_dal.Data;
+using fakeLook_models.Models;
+using fakeLook_starter.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace fakeLook_starter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserRepository _repository;
+        public UserController(DataContext context)
+        {
+            _repository = new UserRepository(context);
+        }
+
+
+        // GET: api/<UsersController>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            //return new string[] { "value1", "value2" };
+            return _repository.GetAll();
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            //return "value";
+            return _repository.GetById(id);
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public void Post([FromBody] User value)
+        {
+            _ = _repository.Add(value);
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] User value)
+        {
+            _ = _repository.Edit(value);
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ fakeLook-starter/Repositories/UserRepository.cs
@@ -0,0 +1,53 @@
+﻿using fakeLook_dal.Data;
+using fakeLook_models.Models;
+using fakeLook_starter.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fakeLook_starter.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        readonly private DataContext _context;
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<User> Add(User item)
+        {
+            //throw new NotImplementedException();
+            var res = _context.Users.Add(item);
+            await _context.SaveChangesAsync();
+            return res.Entity;
+        }
+
+        public async Task<User> Edit(User item)
+        {
+            var user = _context.Users.FirstOrDefault(u => item.Id == u.Id);
+            if (user == null) return null;
+            _context.Entry<User>(user).CurrentValues.SetValues(item);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public ICollection<User> GetAll()
+        {
+            //throw new NotImplementedException();
+            return _context.Users.ToList();
+        }
+
+        public User GetById(int id)
+        {
+            return _context.Users.SingleOrDefault(p => p.Id == id);
+        }
+
+        public ICollection<User> GetByPredicate(Func<User, bool> predicate)
+        {
+            //throw new NotImplementedException();
+            return _context.Users.Where(predicate).ToList();
+
+        }
+    }
+}
